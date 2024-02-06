36acc9b89e673f8d92af6318fd10f76614bfc08c
Lucas Cordero <lucascordero1997@gmail.com>
2024-02-06 18:36:27 -0300

    Add vercel to config.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'static',
-  adapter: vercel({
-    imageService: true,
-  })
+  output: 'server',
+  adapter: vercel(),
 });
