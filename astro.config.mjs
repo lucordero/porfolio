6b5a4fa93acee93ff19d6b4a7d2cb477292108c1
Lucas Cordero <lucascordero1997@gmail.com>
2024-02-06 18:32:30 -0300

    Changes config.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  output: 'static',
   adapter: vercel({
     imageService: true,
   })
